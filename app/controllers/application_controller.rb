9dd69ceadcfcbd5a8d16aebb9240351312abbc77
Andrew Santos <asantos@alumni.nd.edu>
2019-08-07 12:17:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end 
+  
+
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  post '/recipes' do
+    r = Recipe.create(params)
+    redirect "/recipes/#{r.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    recipe.update(params)
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    Recipe.destroy(params[:id])
+    redirect '/recipes'
+  end
+
 
 end
